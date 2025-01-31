4ae104dd2ac09dba4a91ab22f6fc3a7a565f1154
Varvara Tsishchanka <barbara_1798@mail.ru>
2025-01-31 23:05:12 +0300

    project created

---
+++ src/main/java/task1test1/driver_init/RegistrationTest.java
@@ -0,0 +1,16 @@
+package task1test1.driver_init;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class RegistrationTest {
+    private WebDriver driver;
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://qa-course-01.andersenlab.com/registration");
+    }
+}
