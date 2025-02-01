e436fec806b5ee9a68128fd67382600bb5d9c04d
Varvara Tsishchanka <barbara_1798@mail.ru>
2025-02-01 15:37:45 +0300

    project created

--- src/main/java/task1test1/driver_init/RegistrationTestFail.java
+++ src/main/java/task1test1/driver_init/RegistrationTestFail.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-public class RegistrationTest {
+public class RegistrationTestFail {
     private WebDriver driver;
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
@@ -14,14 +14,14 @@ public class RegistrationTest {
         driver.get("https://qa-course-01.andersenlab.com/registration");
         WebElement firstNameField = driver.findElement(By.name("firstName"));
         firstNameField.click();
-        firstNameField.sendKeys("Мария");
+        firstNameField.sendKeys("Ольга");
         WebElement lastNameField = driver.findElement(By.name("lastName"));
-        lastNameField.sendKeys("Мищенко");
+        lastNameField.sendKeys("Романова");
         WebElement dobField = driver.findElement(By.name("dateOfBirth"));
         dobField.sendKeys("01/01/1990");
         dobField.sendKeys(Keys.RETURN);
         WebElement emailField = driver.findElement(By.name("email"));
-        emailField.sendKeys("maria_16966665679797@mail.ru");
+        emailField.sendKeys("olga_165677mail.ru");
         WebElement passwordField = driver.findElement(By.name("password"));
         passwordField.sendKeys("a12345678");
         WebElement passwordConfirmationField = driver.findElement(By.name("passwordConfirmation"));
@@ -33,5 +33,3 @@ public class RegistrationTest {
         driver.quit();
     }
 }
-
-
